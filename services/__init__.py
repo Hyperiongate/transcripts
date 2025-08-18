793c759edb6d112f422c264e60bebf10c06b6edf
Hyperiongate <jim@shift-work.com>
2025-08-18 11:35:08 -0700

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -2,7 +2,7 @@
 Services package for Transcript Fact Checker
 """
 from .transcript import TranscriptProcessor
-from .claims import ClaimsExtractor  # Changed from ClaimExtractor to ClaimsExtractor
+from .claims import ClaimExtractor
 from .factcheck import FactChecker
 
-__all__ = ['TranscriptProcessor', 'ClaimsExtractor', 'FactChecker']  # Also fixed here
+__all__ = ['TranscriptProcessor', 'ClaimExtractor', 'FactChecker']
